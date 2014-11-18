7c4ef68cc89147e3bfaeff628812f664a6e84a15
Heiko Seeberger <mail@heikoseeberger.de>
2014-11-18 17:08:18 +0100

    Use project settings for conductor URL and timeout

--- src/main/scala/com/typesafe/reactiveruntime/sbt/SbtReactiveRuntime.scala
+++ src/main/scala/com/typesafe/reactiveruntime/sbt/SbtReactiveRuntime.scala
@@ -4,25 +4,25 @@
 
 package com.typesafe.reactiveruntime.sbt
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.{ ActorRef, ActorSystem }
 import akka.http.Http
-import akka.http.model.{Uri => HttpUri}
+import akka.http.model.{ Uri => HttpUri }
 import akka.pattern.ask
 import akka.util.Timeout
 import com.typesafe.reactiveruntime.ConductorController
-import com.typesafe.reactiveruntime.ConductorController.{LoadBundle, StartBundle, StopBundle, UnloadBundle}
+import com.typesafe.reactiveruntime.ConductorController.{ LoadBundle, StartBundle, StopBundle, UnloadBundle }
 import com.typesafe.reactiveruntime.console.Console
 import com.typesafe.sbt.bundle.SbtBundle
 import com.typesafe.sbt.packager.Keys._
-import org.scalactic.{Accumulation, Bad, Good, One, Or}
-import play.api.libs.json.{JsString, Json}
+import org.scalactic.{ Accumulation, Bad, Good, One, Or }
+import play.api.libs.json.{ JsString, Json }
 import sbt.Keys._
 import sbt._
 import sbt.complete.DefaultParsers._
 import sbt.complete.Parser
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-import scala.util.{Failure, Success}
+import scala.util.{ Failure, Success }
 
 object Import {
 
@@ -38,7 +38,7 @@ object Import {
     val roles = SettingKey[Set[String]]("rr-roles", "The types of node in the cluster that this bundle can be deployed to.")
 
     val discoveredDist = TaskKey[File]("rr-discovered-dist", "Any distribution produced by the current project")
-    val conductorAddress = SettingKey[URI]("rr-conductor-address", "The location of the conductor. Defaults to 'http://127.0.0.1:9005'.")
+    val conductorUrl = SettingKey[URL]("rr-conductor-url", "The URL of the Conductor. Defaults to 'http://${HOSTNAME}:9005' (if HOSTNAME is defined) or  'http://127.0.0.1:9005'")
     val conductorConnectTimeout = SettingKey[Timeout]("rr-conductor-connect-timeout", "The timeout for conductor communications when connecting")
     val conductorLoadTimeout = SettingKey[Timeout]("rr-conductor-load-timeout", "The timeout for conductor communications when loading")
     val conductorRequestTimeout = SettingKey[Timeout]("rr-conductor-request-timeout", "The timeout for conductor communications when requesting")
@@ -60,24 +60,26 @@ object SbtReactiveRuntime extends AutoPlugin {
   override def requires: Plugins =
     plugins.CorePlugin
 
-  override def globalSettings: Seq[Setting[_]] = super.globalSettings ++ List(
-    onLoad in Global := (onLoad in Global).value andThen loadActorSystem andThen loadWatchdog,
-    onUnload in Global := (onUnload in Global).value andThen unloadWatchdog andThen unloadActorSystem
-  )
+  override def globalSettings: Seq[Setting[_]] =
+    super.globalSettings ++ List(
+      onLoad := onLoad.value.andThen(loadActorSystem).andThen(loadConductorController),
+      onUnload := (unloadConductorController _).andThen(unloadActorSystem).andThen(onUnload.value)
+    )
 
-  override def projectSettings: Seq[Setting[_]] = List(
-    commands += bundleInfo,
-    discoveredDist <<= (dist in ReactiveRuntime).storeAs(discoveredDist in Global).triggeredBy(dist in ReactiveRuntime),
-    loadBundle := loadBundleTask.value.evaluated,
-    roles := Set.empty,
-    startBundle := startBundleTask.value.evaluated,
-    stopBundle := stopBundleTask.value.evaluated,
-    unloadBundle := unloadBundleTask.value.evaluated,
-    conductorAddress := new URI(s"http://${Option(System.getenv("HOSTNAME")).getOrElse("127.0.0.1")}:9005"),
-    conductorConnectTimeout := 30.seconds,
-    conductorLoadTimeout := 10.minutes,
-    conductorRequestTimeout := 30.seconds
-  )
+  override def projectSettings: Seq[Setting[_]] =
+    List(
+      commands += bundleInfo,
+      discoveredDist <<= (dist in ReactiveRuntime).storeAs(discoveredDist in Global).triggeredBy(dist in ReactiveRuntime),
+      loadBundle := loadBundleTask.value.evaluated,
+      roles := Set.empty,
+      startBundle := startBundleTask.value.evaluated,
+      stopBundle := stopBundleTask.value.evaluated,
+      unloadBundle := unloadBundleTask.value.evaluated,
+      conductorUrl := new URL(s"http://${Option(System.getenv("HOSTNAME")).getOrElse("127.0.0.1")}:9005"),
+      conductorConnectTimeout := 30.seconds,
+      conductorRequestTimeout := 30.seconds,
+      conductorLoadTimeout := 10.minutes
+    )
 
   // Input parsing and action
 
@@ -101,19 +103,18 @@ object SbtReactiveRuntime extends AutoPlugin {
   }
 
   private def bundleInfo = Command.command("bundleInfo") { state =>
-    withActorSystem(state)(withWatchdog(state)(Console.bundleInfo))
+    withActorSystem(state)(withConductorController(state)(Console.bundleInfo))
     state
   }
 
   private def loadBundleTask: Def.Initialize[InputTask[String]] =
     Def.inputTask {
-      implicit val timeout = conductorLoadTimeout.value
       def get[A](key: SettingKey[A]) =
         Project.extract(state.value).getOpt(key)
           .fold(Bad(One(s"Setting ${key.key.label} must be defined!")): A Or One[String])(Good(_))
       def loadBundle(nrOfCpus: Double, memory: Long, diskSpace: Long) = {
         val (bundle, config) = Parsers.loadBundle.parsed
-        withWatchdog(state.value) { conductor =>
+        withConductorController(state.value) { conductor =>
           streams.value.log.info("Loading bundle to conductor...")
           val request =
             LoadBundle(
@@ -124,7 +125,7 @@ object SbtReactiveRuntime extends AutoPlugin {
               diskSpace,
               roles.value
             )
-          val response = (conductor ? request).mapTo[String]
+          val response = conductor.ask(request)(conductorLoadTimeout.value).mapTo[String]
           Await.ready(response, conductorLoadTimeout.value.duration)
           response.value.get match {
             case Success(s) =>
@@ -148,11 +149,10 @@ object SbtReactiveRuntime extends AutoPlugin {
 
   private def startBundleTask: Def.Initialize[InputTask[String]] =
     Def.inputTask {
-      implicit val timeout = conductorRequestTimeout.value
       val (bundleId, scale) = Parsers.startBundle.parsed
-      withWatchdog(state.value) { conductor =>
+      withConductorController(state.value) { conductor =>
         streams.value.log.info(s"Starting bundle $bundleId...")
-        val response = (conductor ? StartBundle(bundleId, scale.getOrElse(1))).mapTo[String]
+        val response = conductor.ask(StartBundle(bundleId, scale.getOrElse(1)))(conductorRequestTimeout.value).mapTo[String]
         Await.ready(response, conductorRequestTimeout.value.duration)
         response.value.get match {
           case Success(requestId) =>
@@ -166,9 +166,8 @@ object SbtReactiveRuntime extends AutoPlugin {
 
   private def stopBundleTask: Def.Initialize[InputTask[String]] =
     Def.inputTask {
-      implicit val timeout = conductorRequestTimeout.value
       val bundleId = Parsers.stopBundle.parsed
-      withWatchdog(state.value) { conductor =>
+      withConductorController(state.value) { conductor =>
         streams.value.log.info(s"Stopping all bundle $bundleId instances...")
         val response = conductor.ask(StopBundle(bundleId))(conductorRequestTimeout.value).mapTo[String]
         Await.ready(response, conductorRequestTimeout.value.duration)
@@ -184,11 +183,10 @@ object SbtReactiveRuntime extends AutoPlugin {
 
   private def unloadBundleTask: Def.Initialize[InputTask[String]] =
     Def.inputTask {
-      implicit val timeout = conductorRequestTimeout.value
       val bundleId = Parsers.stopBundle.parsed
-      withWatchdog(state.value) { conductor =>
+      withConductorController(state.value) { conductor =>
         streams.value.log.info(s"Unloading bundle $bundleId...")
-        val response = (conductor ? UnloadBundle(bundleId)).mapTo[String]
+        val response = conductor.ask(UnloadBundle(bundleId))(conductorRequestTimeout.value).mapTo[String]
         Await.ready(response, conductorRequestTimeout.value.duration)
         response.value.get match {
           case Success(requestId) =>
@@ -202,56 +200,59 @@ object SbtReactiveRuntime extends AutoPlugin {
 
   // Actor system management and API
 
-  private val actorSystemAttrKey = AttributeKey[ActorSystem]("sbt-rr-actor-system")
+  private val actorSystemAttrKey = AttributeKey[ActorSystem]("sbt-reactive-runtime-actor-system")
 
   private def loadActorSystem(state: State): State =
     state.get(actorSystemAttrKey).fold {
-      val webActorSystem = withActorClassloader(ActorSystem("sbt-rr"))
-      state.put(actorSystemAttrKey, webActorSystem)
-    }(as => state)
+      state.log.info(s"Creating actor system and storing it under key [${actorSystemAttrKey.label}]")
+      val system = withActorSystemClassloader(ActorSystem("sbt-reactive-runtime"))
+      state.put(actorSystemAttrKey, system)
+    }(_ => state)
 
   private def unloadActorSystem(state: State): State =
-    state.get(actorSystemAttrKey).fold(state) { as =>
-      as.shutdown()
+    state.get(actorSystemAttrKey).fold(state) { system =>
+      system.shutdown()
       state.remove(actorSystemAttrKey)
     }
 
-  private val conductorAttrKey = AttributeKey[ActorRef]("sbt-conductor")
+  private val conductorAttrKey = AttributeKey[ActorRef]("sbt-reactive-runtime-conductor")
 
-  private def loadWatchdog(state: State): State =
+  private def loadConductorController(state: State): State =
     state.get(conductorAttrKey).fold {
+      state.log.info(s"Creating ConductorController actor and storing it under key [${conductorAttrKey.label}]")
       val conductor = withActorSystem(state) { implicit system =>
         val extracted = Project.extract(state)
         val settings = extracted.structure.data
-        val conductor: Option[ActorRef] = for {
-          address <- (conductorAddress in extracted.currentRef).get(settings)
-          connectTimeout <- (conductorConnectTimeout in extracted.currentRef).get(settings)
-        } yield system.actorOf(ConductorController.props(HttpUri(address.toString), connectTimeout, akka.io.IO(Http)))
+        val conductor =
+          for {
+            url <- (conductorUrl in extracted.currentRef).get(settings)
+            connectTimeout <- (conductorConnectTimeout in extracted.currentRef).get(settings)
+          } yield system.actorOf(ConductorController.props(HttpUri(url.toString), connectTimeout, akka.io.IO(Http)))
         conductor.getOrElse(sys.error("Cannot establish the conductor actor. Check that you have conductorAddress and conductorConnectTimeout settings."))
       }
       state.put(conductorAttrKey, conductor)
     }(as => state)
 
-  private def unloadWatchdog(state: State): State =
+  private def unloadConductorController(state: State): State =
     state.get(conductorAttrKey).fold(state)(_ => state.remove(conductorAttrKey))
-
-  // We will get an exception if there is no actor representing the conductor - which is a good thing because
-  // there needs to be and it is probably because the plugin has been mis-configured.
-  private def withWatchdog[T](state: State)(block: ActorRef => T): T =
-    block(state.get(conductorAttrKey).get)
 
   // We will get an exception if there is no known actor system - which is a good thing because
   // there absolutely has to be at this point.
   private def withActorSystem[T](state: State)(block: ActorSystem => T): T =
     block(state.get(actorSystemAttrKey).get)
 
-  private def withActorClassloader[A](f: => A): A = {
+  // We will get an exception if there is no actor representing the conductor - which is a good thing because
+  // there needs to be and it is probably because the plugin has been mis-configured.
+  private def withConductorController[T](state: State)(block: ActorRef => T): T =
+    block(state.get(conductorAttrKey).get)
+
+  private def withActorSystemClassloader[A](action: => A): A = {
     val newLoader = ActorSystem.getClass.getClassLoader
     val thread = Thread.currentThread
     val oldLoader = thread.getContextClassLoader
     thread.setContextClassLoader(newLoader)
     try
-      f
+      action
     finally
       thread.setContextClassLoader(oldLoader)
   }
