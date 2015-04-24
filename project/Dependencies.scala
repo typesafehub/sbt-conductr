196a797581f5864a6a7d12efef54e982ae21a901
Heiko Seeberger <mail@heikoseeberger.de>
2015-04-24 10:11:22 +0200

    Upgrade to sbt-bundle 0.22.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Version {
   val jansi            = "1.11"
   val jline            = "2.12"
   val play             = "2.4.0-M1"
-  val sbtBundle        = "0.21.0"
+  val sbtBundle        = "0.22.0"
   val scalaTest        = "2.2.4"
   val scalactic        = "2.2.4"
 }
