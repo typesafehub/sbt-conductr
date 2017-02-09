f1caea68022518133006ed0542dcbb77d9c07776
Markus Jura <markus.jura@gmail.com>
2017-02-09 13:47:40 +0100

    Fixing tests on Travis

--- install-temp-lightbend-credentials.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir $HOME/.lightbend/ &&
-cat > $HOME/.lightbend/commercial.credentials <<EOL
-realm = Bintray
-host = lightbend.bintray.com
-user = f9095e3b-542c-458a-93e7-418d3218f3fb@lightbend
-password = 2bff26deceadc129a7a52b791d4f68589d56a542
-EOL
