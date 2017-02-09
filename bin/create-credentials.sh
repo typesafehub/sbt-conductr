78680cf94e5b7e160d0109c228abf55760e07c00
Markus Jura <markus.jura@gmail.com>
2017-02-09 11:34:20 +0100

    Add Lightbend credentials to travis

---
+++ bin/create-credentials.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+mkdir ~/.lightbend
+echo "realm = Bintray
+host = dl.bintray.com
+user = e5f80053-61c0-4457-b2e8-05d5ae5d226d@lightbend
+password = cffe8178358500a841d57bc03a35da20d3b7ac98
+" > ~/.lightbend/commercial.credentials
