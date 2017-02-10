22241efd6dd66bd8858eaf250038749967d201b2
James Roper <james@jazzy.id.au>
2017-02-10 17:50:51 +1100

    Added Lagom 1.3 support
    This adds Lagom 1.3 support, including support for the Scala API.
    
    Three new scripted tests have been added to test it.

---
+++ src/sbt-test/lagom-conductr-bundle/lagom-scala-service/lagom-service-impl/src/main/scala/impl/FooLoader.scala
@@ -0,0 +1,32 @@
+package impl
+
+import java.nio.file.{Files, StandardOpenOption}
+import java.util.Date
+
+import com.typesafe.conductr.bundlelib.lagom.scaladsl.ConductRApplicationComponents
+import com.lightbend.lagom.scaladsl.server._
+import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
+import play.api.libs.ws.ahc.AhcWSComponents
+import api.FooService
+
+class FooLoader extends LagomApplicationLoader {
+
+	override def load(context: LagomApplicationContext): LagomApplication =
+		new FooApplication(context) with ConductRApplicationComponents
+
+	override def loadDevMode(context: LagomApplicationContext): LagomApplication =
+		new FooApplication(context) with LagomDevModeComponents
+
+  override def describeServices = List(
+    readDescriptor[FooService]
+  )
+}
+
+abstract class FooApplication(context: LagomApplicationContext)
+	extends LagomApplication(context)
+		with AhcWSComponents {
+
+	override lazy val lagomServer = LagomServer.forServices(
+		bindService[FooService].to(new FooServiceImpl)
+	)
+}
--- src/test/scala/com/lightbend/conductr/sbt/PackageSpec.scala
+++ src/test/scala/com/lightbend/conductr/sbt/PackageSpec.scala
@@ -11,8 +11,8 @@ class PackageSpec extends WordSpec with Matchers {
     }
 
     "return ConductR Lib support for Lagom" in {
-      val result = Library.lagomConductrBundleLib(lagomVersion = "1.0.0", scalaVersion = "2.11", conductrLibVersion = "1.1.3")
-      result shouldBe ("com.typesafe.conductr" % s"lagom1-conductr-bundle-lib_2.11" % "1.1.3")
+      val result = Library.lagomConductrBundleLib(language = "java", lagomVersion = "1.0.0", scalaVersion = "2.11", conductrLibVersion = "1.1.3")
+      result shouldBe ("com.typesafe.conductr" % s"lagom1-java-conductr-bundle-lib_2.11" % "1.1.3")
     }
   }
 }
