9d4b33d574dac3b45e3041daa17480f966a49def
Markus Jura <markus.jura@gmail.com>
2017-02-17 18:25:52 +0100

    Remove cassandra configuration directories
    This PR removes:
    - The cassandra configuration directory `cassandra-conf` from the custom bundle configuration
    - The `runtime-config.sh` from the custom bundle configuration
    
    Both are not necessary with the latest `conductr-cassandra` bundle. The custom bundle configuration, that is produced with the `cassandra-configuration:dist` command on each Lagom project, only need to contain a `bundle.conf`.
    
    As a result, we do not need to maintain the cassandra configuration files in `conductr-cassandra` and `sbt-conductr` whenever we bump the cassandra version.
    
    Tested with the newest `conductr-cassandra` bundle in the sandbox.

--- src/main/resources/bundle-configuration/cassandra-services/runtime-config.sh
+++
@@ -1,3 +0,0 @@
-# Setting Cassandra configuration directory to the bundle configuration directory
-BUNDLE_CONFIG_DIR="$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )"
-export CASSANDRA_CONF=$BUNDLE_CONFIG_DIR/cassandra-conf
