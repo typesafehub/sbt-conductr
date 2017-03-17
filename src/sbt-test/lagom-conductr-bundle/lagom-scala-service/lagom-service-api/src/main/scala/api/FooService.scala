bd854fe6e23b3ee9f72286607a953148e9f1420a
Ignasi Marimon-Clos <ignasi35@gmail.com>
2017-03-17 14:55:50 +0100

    Adds automated tests to start a scala lagom service and use it

--- src/sbt-test/lagom-conductr-bundle/lagom-scala-service/lagom-service-api/src/main/scala/api/FooService.scala
+++ src/sbt-test/lagom-conductr-bundle/lagom-scala-service/lagom-service-api/src/main/scala/api/FooService.scala
@@ -1,20 +1,18 @@
 package api
 
 import akka.NotUsed
-import com.lightbend.lagom.scaladsl.api.ServiceCall
-import com.lightbend.lagom.scaladsl.api.Descriptor
-import com.lightbend.lagom.scaladsl.api.Service
 import com.lightbend.lagom.scaladsl.api.transport.Method
+import com.lightbend.lagom.scaladsl.api.{Service, ServiceCall}
 
 trait FooService extends Service {
 
-  def foo: ServiceCall[NotUsed, NotUsed]
+  def foo: ServiceCall[NotUsed, String]
 
-  override def descriptor = {
-    import Service._
+  import Service._
 
+  override def descriptor =
     named("fooservice").withCalls(
       restCall(Method.GET, "/foo", foo)
     ).withAutoAcl(true)
-  }
+
 }
--- src/sbt-test/lagom-conductr-bundle/lagom-scala-service/lagom-service-impl/src/main/scala/impl/FooServiceImpl.scala
+++ src/sbt-test/lagom-conductr-bundle/lagom-scala-service/lagom-service-impl/src/main/scala/impl/FooServiceImpl.scala
@@ -1,12 +1,12 @@
 package impl
 
-import akka.NotUsed
-import com.lightbend.lagom.scaladsl.api.ServiceCall
-import scala.concurrent.Future
 import api.FooService
+import com.lightbend.lagom.scaladsl.api.ServiceCall
+
+import scala.concurrent.Future
 
 class FooServiceImpl extends FooService {
   override def foo = ServiceCall { _ =>
-    Future.successful(NotUsed)
+    Future.successful("foo-response-hardcoded in FooServiceImpl")
   }
 }
